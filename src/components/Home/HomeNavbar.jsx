07951cb2c3bf13b2e5fc802dd8b6b0ba06646136
Utkirov Boburbek <boburbekutkirov10@gmail.com>
2022-07-18 17:46:18 +0500

    v2

--- src/components/Home/HomeNavbar.jsx
+++ src/components/Home/HomeNavbar.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Icon } from 'react-icons-kit'
+import { menu } from 'react-icons-kit/feather/menu'
+import { x } from 'react-icons-kit/feather/x'
+const HomeNavbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const toggleNav = () => {
+    setToggleMenu((e) => !e)
+  }
 
-export default function HomeNavbar() {
+
+
   return (
-    <div className='col-12 homeNavbar'>HomeNavbar</div>
-  )
-}
+    <div className={ toggleMenu ? 'HomeNavbar col-12 expanded' : 'HomeNavbar'}>
+      <div className='logo'> <span> Magistr.edu.uz</span></div>
+
+      <ul>
+        <li>Asosiy</li>
+        <li>Imtiyozlar</li>
+        <li>Yangiliklar</li>
+        <li>Statistika</li>
+        <button className='btn1'>Kirish </button>
+      </ul>
+
+      <div className="toggle-icon" onClick={toggleNav}>
+        {toggleMenu ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
+      </div>
+
+    </div>
+  );
+};
+
+export default HomeNavbar;
