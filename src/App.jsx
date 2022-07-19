3920ae9de15c121d13f783d10bb4704452a644ef
Utkirov Boburbek <boburbekutkirov10@gmail.com>
2022-07-19 16:32:53 +0500

    v8

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "../node_modules/bootstrap/js/src/modal";
 import Home from "./pages/Home";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Account from "./pages/Account"
 import FullInfo from './components/Account/FullIinfo';
 import Application from './pages/Application'
@@ -18,25 +18,25 @@ import PrivilegeInform from './components/Entrance/PrivilegeInform';
 export default function App() {
   return (
     <div className='container'>
-    <BrowserRouter>
-    <div className="row"><Navbar/></div>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/entrance" element={<Entrance/>}>
-<Route path="" element={<EntryPage/>}></Route>
-<Route path="personal" element={<PersonalInform/>}></Route>
-<Route path="educations" element={<EducationalInform/>}></Route>
-<Route path="specialty" element={<ChoosenSpecialty/>}></Route>
-<Route path="privilege" element={<PrivilegeInform/>}></Route>
-      </Route>
-      <Route path="/account" element={<Account/>}>
-      <Route path="" element={<Application/>}></Route>
-      <Route path="full-info" element={<FullInfo/>}></Route>
-      </Route>
-      
-    </Routes>
-    <div className="row"><Footer/></div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="row"><Navbar /></div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/entrance" element={<Entrance />}>
+            <Route path="" element={<EntryPage />}></Route>
+            <Route path="personal" element={<PersonalInform />}></Route>
+            <Route path="educations" element={<EducationalInform />}></Route>
+            <Route path="specialty" element={<ChoosenSpecialty />}></Route>
+            <Route path="privilege" element={<PrivilegeInform />}></Route>
+          </Route>
+          <Route path="/account" element={<Account />}>
+            <Route path="" element={<Application />}></Route>
+            <Route path="full-info" element={<FullInfo />}></Route>
+          </Route>
+
+        </Routes>
+        <div className="row"><Footer /></div>
+      </BrowserRouter>
     </div>
   )
 }
